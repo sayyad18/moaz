a30f774400e5c8fdcddd0ed791f00c595c63b32c
sayyad18 <105210509+sayyad18@users.noreply.github.com>
2022-05-09 17:35:28 +0200

    js

---
+++ js/style.js
@@ -0,0 +1,19 @@
+  function myFunction() { 
+                      alert("You have successfully completed the form.");
+                        }
+  function mFunction() { 
+                      alert(" This Product Is Sold Out");
+                        }
+
+  function message() {
+                       alert("your feedback has been submitted ");
+                     }
+  function min() {
+                    alert("Email:mariamellithy22@gmail.com");
+                 }
+  function Max() {
+                   alert("Thanks for visiting us");
+                 }
+  function bclick(){
+                     alert("Rate us");
+                    }
